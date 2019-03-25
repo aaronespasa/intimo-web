928ceb107c234ff99b11fb826acda6dd913cab50
Adrian <adrianmartinezr2002@gmail.com>
2019-03-25 20:02:28 +0100

    Eliminado acceso a la base de datos

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); //Defines objects with a strong typed diagram which is asigned to MongoDB document
 
-mongoose.connect('mongodb+srv://adrimr02:starTREK1966@proyectos-aukjk.mongodb.net/test?retryWrites=true', {
+mongoose.connect('mongodb://localhost/intimo-db-app', {
   useFindAndModify: false, //Use `findOneAndUpdate()`
   useCreateIndex: true, //Use createIndex() instead of ensureIndex()
   useNewUrlParser: true //Rewrote the tool it uses to parse MongoDB connection strings
