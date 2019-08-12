69c5871ce3985fe1bf70b976b51f3d10aad12ad4
Adrian <adrianmartinezr2002@gmail.com>
2019-08-13 00:29:43 +0200

    Mas fallos :)

--- models/Products.js
+++ models/Products.js
@@ -8,18 +8,11 @@ const productSchema = new Schema({
     age: { type: String, required: true },
     type: {type: String, requires: true},
     //imagen
-<<<<<<< HEAD
     filename: { type: String, require}, 
     path: {type: String, required: true},
     originalname: { type: String, required: true },
     //sizes: { type: Array, require: true },
     created_at: { type: Date, default: Date.now }
-=======
-    filename: { type: String, require},
-    path: {type: String, required: true},
-    originalname: { type: String, required: true }
-    //sizes: { type: Array, require: true },
->>>>>>> 40f8b69bde0d5dd6c3e8bff4b982aa123102810b
 });
 
 module.exports = model('Products', productSchema);
