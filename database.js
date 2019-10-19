324108a9371eb7d9f8a3de6fdf4661d088552182
Adrian <adrianmartinezr2002@gmail.com>
2019-10-20 01:20:10 +0200

    Connection to DBAtlas and .env file

--- database.js
+++ database.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose') // Defines objects with a strong typed diagram which is asigned to MongoDB document
 
-mongoose.connect('mongodb://localhost/intimo-db-app', {
+const { config } = require('./config/index');
+
+const DBUSER = encodeURIComponent(config.dbUser);
+const DBPASSWORD = encodeURIComponent(config.dbPassword);
+const DBHOST = encodeURIComponent(config.dbHost);
+const DBNAME = encodeURIComponent(config.dbName);
+
+mongoose.connect(`mongodb+srv://${DBUSER}:${DBPASSWORD}@${DBHOST}/${DBNAME}?retryWrites=true&w=majority`, {
   useFindAndModify: false, // Use `findOneAndUpdate()`
   useCreateIndex: true, // Use createIndex() instead of ensureIndex()
   useNewUrlParser: true // Rewrote the tool it uses to parse MongoDB connection strings
 })
-  .then(db => console.log('DB is connected'))
+  .then(_ => console.log('DB is connected'))
   .catch(err => console.error(err))
